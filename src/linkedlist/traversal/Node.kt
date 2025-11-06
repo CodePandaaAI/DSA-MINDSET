c0f357dfe8c55a06fc42a5a2069bc7a988dc5024
CodePandaaAI <romitsharmakv@gmail.com>
2025-11-06 15:33:14 +0530

    Refactored `ReverseLinkedList.kt` to include both efficient and inefficient solutions for reversing linked lists; updated the main function for demonstration.

--- src/linkedlist/traversal/Node.kt
+++ src/linkedlist/traversal/Node.kt
@@ -2,8 +2,8 @@ package linkedlist.traversal
 
 class Node<T>(
     var `val`: T,
-    var next: Node<T>? = null
 ) {
+    var next: Node<T>? = null
     override fun toString(): String {
         return if (next != null) {
             "$`val` -> ${next.toString()}"
--- src/linkedlist/traversal/ReverseLinkedList.kt
+++ src/linkedlist/traversal/ReverseLinkedList.kt
@@ -3,20 +3,36 @@ package linkedlist.traversal
 fun main() {
     val linkedList = LinkedList<Int>()
     for (i in 1..5) linkedList.add(i)
+    print(reverseLinkedListEff(head = linkedList.head).toString())
+}
+
+fun reverseLinkedListInEfficient(head: Node<Int>?) {
     val list = mutableListOf<Int>()
 
-    var current = linkedList.head
+    var current = head
 
     while (current != null) {
         list.add(current.`val`)
         current = current.next
     }
-    list.reverse()
-    current = linkedList.head
+    current = head
 
-    for (num in list) {
-        current!!.`val` = num
+    for (i in list.size - 1 downTo 0) {
+        current!!.`val` = list[i]
         current = current.next
     }
-    print(linkedList.toString())
+}
+
+fun reverseLinkedListEff(head: Node<Int>?): Node<Int>? {
+    var prev: Node<Int>? = null
+    var current = head
+    var next: Node<Int>?
+
+    while (current != null) {
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+    return prev
 }
